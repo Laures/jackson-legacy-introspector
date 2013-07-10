7321360e8b7cbb7d9a5415a413b0033ba2bc1324
Neil Gentleman <ngentleman@gmail.com>
2013-07-09 23:16:45 -0700

    implement more of JsonParser2To1Wrapper
    only some of the new methods are covered by TestArrayOrStringDeserializer.

--- src/main/java/net/bigpoint/jackson/databind/wrapper/JsonParser2To1Wrapper.java
+++ src/main/java/net/bigpoint/jackson/databind/wrapper/JsonParser2To1Wrapper.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Iterator;
 
+import com.fasterxml.jackson.core.io.SerializedString;
 import org.codehaus.jackson.Base64Variant;
 import org.codehaus.jackson.JsonLocation;
 import org.codehaus.jackson.JsonParseException;
@@ -18,6 +19,8 @@ import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.JsonStreamContext;
 import org.codehaus.jackson.JsonToken;
 import org.codehaus.jackson.ObjectCodec;
+import org.codehaus.jackson.SerializableString;
+import org.codehaus.jackson.type.TypeReference;
 
 /**
  * @author Alexander
@@ -62,8 +65,7 @@ public class JsonParser2To1Wrapper extends JsonParser {
 
 	@Override
 	public byte[] getBinaryValue(Base64Variant b64variant) throws IOException, JsonParseException {
-		// TODO Auto-generated method stub
-		return null;
+		return wrappedParser.getBinaryValue(JacksonTransformers.transformBase64Variant(b64variant));
 	}
 
 	@Override
@@ -74,7 +76,7 @@ public class JsonParser2To1Wrapper extends JsonParser {
 	@Override
 	public JsonToken nextToken() throws IOException, JsonParseException {
 		try {
-			return JsonToken.valueOf(wrappedParser.nextToken().name());
+			return JacksonTransformers.transformJsonToken(wrappedParser.nextToken());
 		} catch (com.fasterxml.jackson.core.JsonParseException e) {
 			throw wrapJsonParseException(e);
 		}
@@ -166,7 +168,7 @@ public class JsonParser2To1Wrapper extends JsonParser {
 	@Override
 	public NumberType getNumberType() throws IOException, JsonParseException {
 		try {
-			return NumberType.valueOf(wrappedParser.getNumberType().name());
+			return JacksonTransformers.transformNumberType(wrappedParser.getNumberType());
 		} catch (com.fasterxml.jackson.core.JsonParseException e) {
 			throw wrapJsonParseException(e);
 		}
@@ -233,19 +235,19 @@ public class JsonParser2To1Wrapper extends JsonParser {
 
 	@Override
 	public JsonParser configure(Feature f, boolean state) {
-		wrappedParser = wrappedParser.configure(com.fasterxml.jackson.core.JsonParser.Feature.valueOf(f.name()), state);
+		wrappedParser = wrappedParser.configure(JacksonTransformers.transformFeature(f), state);
 		return this;
 	}
 
 	@Override
 	public JsonParser disable(Feature f) {
-		wrappedParser = wrappedParser.disable(com.fasterxml.jackson.core.JsonParser.Feature.valueOf(f.name()));
+		wrappedParser = wrappedParser.disable(JacksonTransformers.transformFeature(f));
 		return this;
 	}
 
 	@Override
 	public JsonParser enable(Feature f) {
-		wrappedParser = wrappedParser.enable(com.fasterxml.jackson.core.JsonParser.Feature.valueOf(f.name()));
+		wrappedParser = wrappedParser.enable(JacksonTransformers.transformFeature(f));
 		return this;
 	}
 
@@ -262,6 +264,220 @@ public class JsonParser2To1Wrapper extends JsonParser {
 	public <T> T readValueAs(Class<T> valueType) throws IOException, JsonProcessingException {
 		try {
 			return wrappedParser.readValueAs(valueType);
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public boolean getValueAsBoolean(boolean defaultValue) throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getValueAsBoolean(defaultValue);
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public JsonToken nextValue() throws IOException, JsonParseException {
+		try {
+			return JacksonTransformers.transformJsonToken(wrappedParser.nextValue());
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public boolean nextFieldName(SerializableString str) throws IOException, JsonParseException {
+		try {
+			return wrappedParser.nextFieldName(new SerializedString(str.getValue()));
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public String nextTextValue() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.nextTextValue();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public int nextIntValue(int defaultValue) throws IOException, JsonParseException {
+		try {
+			return wrappedParser.nextIntValue(defaultValue);
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public long nextLongValue(long defaultValue) throws IOException, JsonParseException {
+		try {
+			return wrappedParser.nextLongValue(defaultValue);
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public Boolean nextBooleanValue() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.nextBooleanValue();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public JsonToken getCurrentToken() {
+		return JacksonTransformers.transformJsonToken(wrappedParser.getCurrentToken());
+	}
+
+	@Override
+	public boolean hasCurrentToken() {
+		return wrappedParser.hasCurrentToken();
+	}
+
+	@Override
+	public JsonToken getLastClearedToken() {
+		return JacksonTransformers.transformJsonToken(wrappedParser.getLastClearedToken());
+	}
+
+	@Override
+	public boolean isExpectedStartArrayToken() {
+		return wrappedParser.isExpectedStartArrayToken();
+	}
+
+	@Override
+	public boolean hasTextCharacters() {
+		return wrappedParser.hasTextCharacters();
+	}
+
+	@Override
+	public byte getByteValue() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getByteValue();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public short getShortValue() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getShortValue();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public boolean getBooleanValue() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getBooleanValue();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public Object getEmbeddedObject() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getEmbeddedObject();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public byte[] getBinaryValue() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getBinaryValue();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public int getValueAsInt() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getValueAsInt();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public int getValueAsInt(int defaultValue) throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getValueAsInt(defaultValue);
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public long getValueAsLong() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getValueAsLong();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public long getValueAsLong(long defaultValue) throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getValueAsLong(defaultValue);
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public double getValueAsDouble() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getValueAsDouble();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public double getValueAsDouble(double defaultValue) throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getValueAsDouble(defaultValue);
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public boolean getValueAsBoolean() throws IOException, JsonParseException {
+		try {
+			return wrappedParser.getValueAsBoolean();
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public <T> T readValueAs(TypeReference<?> valueTypeRef) throws IOException, JsonProcessingException {
+		try {
+			return wrappedParser.readValueAs(JacksonTransformers.transformTypeReference(valueTypeRef));
+		} catch (com.fasterxml.jackson.core.JsonParseException e) {
+			throw wrapJsonParseException(e);
+		}
+	}
+
+	@Override
+	public <T> Iterator<T> readValuesAs(TypeReference<?> valueTypeRef) throws IOException, JsonProcessingException {
+		try {
+			return wrappedParser.readValuesAs(JacksonTransformers.transformTypeReference(valueTypeRef));
 		} catch (com.fasterxml.jackson.core.JsonParseException e) {
 			throw wrapJsonParseException(e);
 		}
